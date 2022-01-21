e2235230729b48445c94d94ca0453f29a5f9d60b
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2022-01-21 14:19:13 +0100

    handlebars fix

--- index.js
+++ index.js
@@ -1,32 +1,49 @@
 import express from "express";
 import { engine } from "express-handlebars";
-import { marked } from "marked";
+import { marked } from "marked"; 
 import { loadMovie, loadMovies } from "./script/movies.js";
 
 const app = express();
 
-app.engine("handlebars", engine({
-    helpers: {
-        markdown: md => marked(md),
-    },
-}));
+ app.engine("handlebars", engine({ 
+     helpers: { 
+         markdown: md => marked(md), 
+     },
+ }));
+ 
 app.set("view engine", "handlebars");
 app.set("views","./frame");
 
 app.get("/", async(req,res) => {
+    res.render("./partials/home");
+});
+
+
+ app.get("/about", async(req,res) => {
+    res.render("./partials/about");
+}); 
+
+app.get("/contact", async(req,res) => {
+    res.render("./partials/contact");
+});
+
+app.get("/movies", async(req,res) => {
     const movies = await loadMovies();
-    res.render("home",{ movies });
+    res.render("./partials/allMovies", { movies });
 });
 
 app.get("/movies/:movieId", async(req, res) => {
-    const movie = await loadMovie(req.params.movieId);
-    if(movie) {
-        res.render("movie", { movie });
+ const movie = await loadMovie(req.params.movieId);
+ if(movie) {
+        res.render("./partials/movie",{ movie });
     } else {
-        res.status(404).render("404");
-    }
+        res.status(404).render("./partials/404");
+    } 
 });
 
-app.use("/style", express.static("./style"));
+
+app.use("/static", express.static("./static"));
 
 app.listen(5080);
+
+
